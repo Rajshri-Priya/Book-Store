5c89e834f53f31a39427d3e0e8b4cd3a1a418e85
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-03-14 12:22:26 +0530

    [Rajshri Priya].Added register,login and logout views

---
+++ user_auth/views.py
@@ -0,0 +1,66 @@
+from bookstore.user_auth.serializers import LoginSerializer, RegistrationSerializer
+from django.contrib.auth import login, logout
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+
+class RegistrationAPIView(APIView):
+    """
+         Class is to register for the user
+    """
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        try:
+            serializer = RegistrationSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response({"success": True, "message": "user Registered Successfully", "data": serializer.data,
+                             "status": 201}, status=201)
+
+        except Exception as e:
+            # logger.exception(e)
+            return Response({"success": False, "message": str(e), "status": 400}, status=400)
+
+class LoginAPIView(APIView):
+    """
+        This class is used for the User login
+    """
+    serializer_class = LoginSerializer
+
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    # @swagger_auto_schema(request_body=LoginSerializer, operation_summary='POST User Login')
+    def post(self, request):
+        try:
+            serializer = LoginSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            user = serializer.context.get('user')
+            login(request, user)
+            return Response({"success": True, "message": "Login Successfully", "user": user, "status": 201},
+                            status=201)
+        except Exception as e:
+            # logger.exception(e)
+            return Response({"success": False, "message": str(e), "status": 401}, status=401)
+
+class LogoutView(APIView):
+    """
+        This class is used for the User logout
+    """
+    def get(self, request):
+        try:
+            # Check if user is authenticated
+            if request.user.is_authenticated:
+                # Logout user
+                logout(request)
+                # Redirect to login page
+                return Response({'message': 'logout successfully.'})
+            else:
+                return Response({'message': 'You are not logged in.'})
+        except Exception as e:
+            # logger.exception(e)
+            return Response({'message': 'An error occurred during logout: {}'.format(str(e))})

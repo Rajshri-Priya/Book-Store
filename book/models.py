eccbb795453cd3e59ca8b2da3afafee5cc53bf98
rajshri-priya <rajshri.priya1708@.gmail.com>
2023-03-14 13:36:53 +0530

    [Rajshri Priya] Added book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from user_auth.models import CustomUser
+
 
 # Create your models here.
+class Book(models.Model):
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
